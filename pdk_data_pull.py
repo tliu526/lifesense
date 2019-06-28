9eece33eb6b63fff2ccf324ee7715a895a582497
Tony Liu <tliu0526@gmail.com>
2019-06-28 13:38:26 -0500

    updates pdk_data_pull.py with additional metadata fields

--- pdk_data_pull.py
+++ pdk_data_pull.py
@@ -25,59 +25,6 @@ client = PDKClient(site_url=SITE_URL, token=TOKEN)
 query = client.query_data_points(page_size=PAGE_SIZE)
 query.count()
 
-#%% test cell for location
-location_query = query.filter(source='23853441', generator_identifier='pdk-location').order_by('created')
-
-first_loc_point = location_query.first()
-loc_df = pd.DataFrame()
-for loc in location_query:
-    loc_df = loc_df.append(pd.read_json(json.dumps(loc)).iloc[0])
-    break
-
-loc_df['pid'] = '23853441'
-print(loc_df.shape)
-display(loc_df.head())
-
-#%%
-# pull and dump locations for testwave ids 
-testwave_ids = []
-with open("data_pull/test_wave_ids.txt", "rb") as testwave_f:
-    for line in testwave_f.readlines():
-        testwave_ids.append(line.strip())
-
-for id in testwave_ids:
-    print(id)
-    location_query = query.filter(source=id, generator_identifier='pdk-location').order_by('created')
-    
-    loc_df = pd.DataFrame()
-    for loc in location_query:
-        loc_df = loc_df.append(pd.read_json(json.dumps(loc)).iloc[0])
-
-    loc_df['pid'] = id 
-    loc_df['data_source'] = 'test_wave'
-    print(loc_df.shape)
-    pickle.dump(loc_df, open("data_pull/loc/{}.df".format(id), 'wb'), -1)
-
-#%%
-# pull and dump locations for internal staff ids
-testwave_ids = []
-with open("data_pull/internal_staff_ids.txt", "rb") as testwave_f:
-    for line in testwave_f.readlines():
-        testwave_ids.append(line.strip())
-
-for id in testwave_ids:
-    print(id)
-    location_query = query.filter(source=id, generator_identifier='pdk-location').order_by('created')
-    
-    loc_df = pd.DataFrame()
-    for loc in location_query:
-        loc_df = loc_df.append(pd.read_json(json.dumps(loc)).iloc[0])
-
-    loc_df['pid'] = id 
-    loc_df['data_source'] = 'internal_staff'
-    print(loc_df.shape)
-    pickle.dump(loc_df, open("data_pull/loc/{}.df".format(id), 'wb'), -1)
-
 #%% test cell for evening EMA processing
 id = '70859397'
 
@@ -107,15 +54,59 @@ ema_df.columns
 #%% test cell for morning EMA processing
 id = '70859397'
 
-ema_query = query.filter(source=id, generator_identifier='morning_phq8').order_by('created')
+ema_query = query.filter(source=id, generator_identifier='morning_ema').order_by('created')
 
 point = ema_query.first()
 ema_df = pd.DataFrame()
 
 ema_df = ema_df.append(pd.DataFrame.from_dict(point).iloc[0])
 
-display(ema_df)
+metadata_dict = point['passive-data-metadata'] 
 
+metadata_df = pd.Series(point['passive-data-metadata']).to_frame().transpose()
+ema_df.reset_index(inplace=True, drop=True)
+full_df = pd.concat([metadata_df, ema_df], axis=1)
+full_df.drop('passive-data-metadata', axis='columns', inplace=True)
+display(full_df)
+
+#%%
+# processing functions
+
+def process_generators(query, ids, generators, data_source):
+    """Processes the data for the given generators and ids.
+
+    Dumps the resulting DataFrames into pickled files.
+
+    Args:
+        query: a PDKPointQuery object
+        ids: list of ids to query
+        generators: list of generators to pull
+        data_source: str indicating which group the ids belong to
+
+    """
+    for id in ids:
+        print(id)
+        
+        for gen_id in generators:
+            ema_query = query.filter(source=id, generator_identifier=gen_id).order_by('created')
+            
+            ema_df = pd.DataFrame()
+            for point in ema_query:
+                point_df = pd.DataFrame.from_dict(point).iloc[0].to_frame().transpose()
+                metadata_df = pd.Series(point['passive-data-metadata']).to_frame().transpose()
+                point_df.reset_index(inplace=True, drop=True)
+                point_df = pd.concat([metadata_df, point_df], axis=1, sort=True)
+                
+                point_df.drop('passive-data-metadata', axis='columns', inplace=True)
+
+                ema_df = ema_df.append(point_df)
+
+            ema_df['pid'] = id 
+            ema_df['data_source'] = data_source
+            ema_df = ema_df.reset_index(drop=True)
+            print(ema_df.shape)
+            #display(ema_df.head())
+            pickle.dump(ema_df, open("data_pull/{}/{}.df".format(gen_id, id), 'wb'), -1)
 
 
 #%% 
@@ -126,25 +117,9 @@ with open("data_pull/test_wave_ids.txt", "rb") as testwave_f:
     for line in testwave_f.readlines():
         testwave_ids.append(line.strip())
 
-#generators = ['morning_phq8', 'evening_phq8']
 generators = ['morning_ema', 'evening_ema']
-for id in testwave_ids:
-    print(id)
+process_generators(query, testwave_ids, generators, 'test_wave')
     
-    for gen_id in generators:
-        ema_query = query.filter(source=id, generator_identifier=gen_id).order_by('created')
-        
-        ema_df = pd.DataFrame()
-        for point in ema_query:
-            ema_df = ema_df.append(pd.DataFrame.from_dict(point).iloc[0])
-
-        ema_df['pid'] = id 
-        ema_df['data_source'] = 'test_wave'
-        ema_df = ema_df.reset_index(drop=True)
-        print(ema_df.shape)
-        #display(ema_df.head())
-        pickle.dump(ema_df, open("data_pull/{}/{}.df".format(gen_id, id), 'wb'), -1)
-
 #%% 
 # process morning/evening EMA for internal testers
 
@@ -153,21 +128,62 @@ with open("data_pull/internal_staff_ids.txt", "rb") as testwave_f:
     for line in testwave_f.readlines():
         internal_ids.append(line.strip())
 
-#generators = ['morning_phq8', 'evening_phq8']
 generators = ['morning_ema', 'evening_ema']
-for id in internal_ids:
-    print(id)
-    
-    for gen_id in generators:
-        ema_query = query.filter(source=id, generator_identifier=gen_id).order_by('created')
-        
-        ema_df = pd.DataFrame()
-        for point in ema_query:
-            ema_df = ema_df.append(pd.DataFrame.from_dict(point).iloc[0])
+process_generators(query, internal_ids, generators, 'internal_staff')
 
-        ema_df['pid'] = id 
-        ema_df['data_source'] = 'test_wave'
-        ema_df = ema_df.reset_index(drop=True)
-        print(ema_df.shape)
-        #display(ema_df.head())
-        pickle.dump(ema_df, open("data_pull/{}/{}.df".format(gen_id, id), 'wb'), -1)
+#%% test cell for location
+location_query = query.filter(source='23853441', generator_identifier='pdk-location').order_by('created')
+
+first_loc_point = location_query.first()
+loc_df = pd.DataFrame()
+for loc in location_query:
+    loc_df = loc_df.append(pd.read_json(json.dumps(loc)).iloc[0])
+    break
+
+loc_df['pid'] = '23853441'
+print(loc_df.shape)
+display(loc_df.head())
+#%%
+# pull and dump locations for internal staff ids
+testwave_ids = []
+with open("data_pull/internal_staff_ids.txt", "rb") as testwave_f:
+    for line in testwave_f.readlines():
+        testwave_ids.append(line.strip())
+
+for id in testwave_ids:
+    print(id)
+    location_query = query.filter(source=id, generator_identifier='pdk-location').order_by('created')
+    
+    loc_df = pd.DataFrame()
+    for point in location_query:
+        point_df = pd.DataFrame.from_dict(point).iloc[0].to_frame().transpose()
+        metadata_df = pd.Series(point['passive-data-metadata']).to_frame().transpose()
+        metadata_df = metadata_df.drop(['latitude', 'longitude'], axis='columns')
+        point_df.reset_index(inplace=True, drop=True)
+        point_df = pd.concat([metadata_df, point_df], axis=1, sort=True)
+        
+        point_df.drop('passive-data-metadata', axis='columns', inplace=True)
+        #print("pre missing cols:{}".format(point_df.shape[1]))
+        missing_cols = [col for col in loc_df.columns.values if col not in point_df.columns.values]
+        #print(missing_cols)
+        
+        if len(missing_cols) > 0 and loc_df.shape[0] > 0:
+            for col in missing_cols:
+                point_df[col] = np.nan
+            point_df = point_df[loc_df.columns]
+        
+        #print("post-missing cols:{}".format(point_df.shape[1]))
+
+        
+        loc_df = loc_df.append(point_df)
+
+        
+    loc_df['pid'] = id 
+    loc_df['data_source'] = 'internal_staff'
+    print(loc_df.shape)
+    display(loc_df.head())
+    
+    pickle.dump(loc_df, open("data_pull/pdk-location/{}.df".format(id), 'wb'), -1)
+
+
+#%%
